34009748d97f5fdf963ede291dd3e0de272c5149
gsornsen <gsornsen@gmail.com>
2022-05-11 22:16:49 -0700

    Python3Packages.typing-extensions: 3.10.0.2 -> 4.2.0

--- pkgs/development/python-modules/typing-extensions/default.nix
+++ pkgs/development/python-modules/typing-extensions/default.nix
@@ -1,14 +1,23 @@
-{ lib, buildPythonPackage, fetchPypi, pythonOlder, isPy3k, python, typing }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, flit-core
+, pythonOlder
+, isPy3k
+, python
+, typing
+}:
+
 let
-  testDir = if isPy3k then "src_py3" else "src_py2";
+  testDir = "src";
 
 in buildPythonPackage rec {
   pname = "typing_extensions";
-  version = "3.10.0.2";
+  version = "4.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "49f75d16ff11f1cd258e1b988ccff82a3ca5570217d7ad8c5f48205dd99a677e";
+    sha256 = "f1c24655a0da0d1b67f07e17a5e6b2a105894e6824b92096378bb3668ef02376";
   };
 
   checkInputs = lib.optional (pythonOlder "3.5") typing;
@@ -16,6 +25,9 @@ in buildPythonPackage rec {
   # Error for Python3.6: ImportError: cannot import name 'ann_module'
   # See https://github.com/python/typing/pull/280
   doCheck = pythonOlder "3.6";
+  propagatedBuildInputs = [ flit-core ];
+
+  format = "pyproject";
 
   checkPhase = ''
     cd ${testDir}
